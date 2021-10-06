ecfc65646489cea2be446ce3d2ed9a4a5779c299
kevint2005 <83583613+kevint2005@users.noreply.github.com>
2021-10-06 10:30:43 +0200

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,14 @@ var username=document.getElementById("us").value;
 var password=document.getElementById("pw").value;
 if(
 username=="Kevin" && password=="Kkevinvip8" || 
-username=="ila" && password=="mamtapecr"){
+username=="ila" && password=="mamtapecr" ||
+username=="mattia" && password=="porro" ||
+username=="andrea" && password=="ricchiuti" ||
+username=="gabriele" && password=="storelli" ||
+username=="santo" && password=="butera" ||
+username=="francesco" && password=="diluzio" ||
+
+){
     window.location.href = 'index2.html';
 }else{
 alert("login failed");
